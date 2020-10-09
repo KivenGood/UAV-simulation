02e69d0daf021e5dd660030fdfe0fe9988e6ef21
kiven <1132232330@qq.com>
2020-10-09 11:35:50 +0800

    Signed-off-by: kiven <1132232330@qq.com>

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {
     username: 'root', // 用户名
     password: 'lz1349500382', // 口令
     host: 'localhost', // 主机名
-    port: 3306, // 端口号，MySQL默认3306
+    port: 3306, //端口号，MySQL默认3306
     database: 'sheet2' // 数据库名，不是表名
 };
 
