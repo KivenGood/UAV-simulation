3f2e12109b0e52584c07d9b08243fc2bab4b3e7a
kiven <1132232330@qq.com>
2020-12-23 19:35:15 +0800

    解耦；

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 var config = {
+    //数据库配置
     // database: 'test', // 使用哪个数据库
     username: 'root', // 用户名
-    password: 'lz1349500382', // 口令
+    password: '111111', // 口令
     host: 'localhost', // 主机名
     port: 3306, //端口号，MySQL默认3306
-    database: 'sheet2' //数据库名，不是表名
+    database: 'sheet2',//数据库名，不是表名.
 };
+//为node.js使用需要暴露
 module.exports = config;
---
+++ sourceConfig.js
@@ -0,0 +1,8 @@
+var sourceConfig = {
+
+    //资源配置
+    mapImagesSource: 'http://localhost:8081/hainanimage/tiles',//瓦片切成地形的图片格式路径
+    mapImagesSourceType: 'png',
+    mapTerrainSource: "http://localhost:8081/hainan",//瓦片资源路径
+    planeMode: 'http://localhost:8081/models/ch.gltf',
+};
